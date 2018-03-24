c2defea8a6f3e67a8fdc25968e2bba0a41e7b62e
CodeingerCat <DBbombed@Gmail.com>
2018-03-24 01:10:35 -0500

    Menu Parts 1
    [Added]
    + Base Menu Class = Base Class for all menues
       > Handels switching between menues mainly
    + Main Menu Class = Override of Base Menu switching to use key to open menu
    + Controlls Menu = For selecting hot keys for player input

---
+++ GDC Robot Puzzle Game/Assets/Scripts/MenuBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuBase : MonoBehaviour {
+    public Button backButton;
+    public MenuBase lastMenue;
+
+    public virtual void Start()
+    {
+        if(backButton)
+        backButton.onClick.AddListener(delegate { Back(); });
+    }
+
+    public void Back()
+    {
+        lastMenue.gameObject.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
